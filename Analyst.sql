27fd4b7e90e6d8213dbd70553d3bf4bc756be80b
nikhilp0799 <pandey.nikh@northeastern.edu>
2024-11-07 22:18:53 -0500

    Analyst select statement

---
+++ Analyst.sql
@@ -0,0 +1,16 @@
+
+----------------------- To view the data of the VIEWS Created please run these queries ------------------------
+
+SELECT * FROM PROJECT_USER.Current_Inventory_Status;
+
+SELECT * FROM PROJECT_USER.Reservation_Revenue_Analysis;
+
+SELECT * FROM PROJECT_USER.Most_Popular_Vehicles;
+
+SELECT * FROM PROJECT_USER.Customer_Complaints;
+
+SELECT * FROM PROJECT_USER.Week_wise_Sales;
+
+SELECT * FROM PROJECT_USER.Total_Sales_by_Region;
+
+SELECT * FROM PROJECT_USER.Product_wise_Price_Changes;
