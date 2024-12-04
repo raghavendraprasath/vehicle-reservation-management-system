2ef796b1d7720baee3834db355eac8fc0a56c528
rohanjauhari1312 <jauhari.r@northeastern.edu>
2024-12-04 00:30:23 -0500

    extra grants given to analyst and project user

--- PROJECT_USER_CREATION_GRANTS.sql
+++ PROJECT_USER_CREATION_GRANTS.sql
@@ -49,11 +49,46 @@ BEGIN
     FOR view_record IN (SELECT view_name FROM all_views WHERE owner = 'PROJECT_USER') LOOP
         EXECUTE IMMEDIATE 'GRANT SELECT ON PROJECT_USER.' || view_record.view_name || ' TO PROJECT_USER WITH GRANT OPTION';
     END LOOP;
+
+    -- Grant EXECUTE ANY PROCEDURE privilege to the user
+    EXECUTE IMMEDIATE 'GRANT EXECUTE ANY PROCEDURE TO PROJECT_USER';
+
+    -- Grant CREATE TRIGGER privilege to the user
+    EXECUTE IMMEDIATE 'GRANT CREATE TRIGGER TO PROJECT_USER';
+
+    -- Grant ADMINISTER DATABASE TRIGGER privilege to the user
+    EXECUTE IMMEDIATE 'GRANT ADMINISTER DATABASE TRIGGER TO PROJECT_USER';
+
+    -- Grant CREATE ANY VIEW privilege to the user
+    EXECUTE IMMEDIATE 'GRANT CREATE ANY VIEW TO PROJECT_USER';
+
+    -- Grant CREATE ANY PROCEDURE privilege to the user
+    EXECUTE IMMEDIATE 'GRANT CREATE ANY PROCEDURE TO PROJECT_USER';
+
+    -- Grant CREATE SYNONYM privilege to the user
+    EXECUTE IMMEDIATE 'GRANT CREATE SYNONYM TO PROJECT_USER';
+
+    -- Grant DROP ANY VIEW privilege to the user
+    EXECUTE IMMEDIATE 'GRANT DROP ANY VIEW TO PROJECT_USER';
+
+    -- Grant CREATE ANY INDEX privilege to the user
+    EXECUTE IMMEDIATE 'GRANT CREATE ANY INDEX TO PROJECT_USER';
+
+    -- Grant ALTER ANY INDEX privilege to the user
+    EXECUTE IMMEDIATE 'GRANT ALTER ANY INDEX TO PROJECT_USER';
+    
+    EXECUTE IMMEDIATE 'CREATE OR REPLACE DIRECTORY reports_dir AS ''/Users/nikhilpandey/Desktop''';
+    
+    EXECUTE IMMEDIATE 'GRANT READ, WRITE ON DIRECTORY reports_dir TO project_user';
+
+
+
 EXCEPTION
     WHEN OTHERS THEN
         NULL; -- Ignore if grants already exist
 END;
 /
+
 
 
 
@@ -101,11 +136,37 @@ BEGIN
 END;
 /
 
--- Grant necessary privileges to the user
 BEGIN
+    -- Grant basic connection privileges
     EXECUTE IMMEDIATE 'GRANT CONNECT, CREATE SESSION TO ANALYST_USER';
+
+    -- Grant SELECT privileges on all relevant views in the PROJECT_USER schema
+    FOR view_record IN (SELECT view_name FROM all_views WHERE owner = 'PROJECT_USER') LOOP
+        EXECUTE IMMEDIATE 'GRANT SELECT ON PROJECT_USER.' || view_record.view_name || ' TO ANALYST_USER';
+    END LOOP;
+
+    -- Grant SELECT privileges on all tables in the PROJECT_USER schema
+    FOR table_record IN (SELECT table_name FROM all_tables WHERE owner = 'PROJECT_USER') LOOP
+        EXECUTE IMMEDIATE 'GRANT SELECT ON PROJECT_USER.' || table_record.table_name || ' TO ANALYST_USER';
+    END LOOP;
+
+    -- Grant EXECUTE privileges on all procedures in the PROJECT_USER schema
+    FOR proc_record IN (SELECT object_name FROM all_objects WHERE object_type IN ('PROCEDURE', 'FUNCTION') AND owner = 'PROJECT_USER') LOOP
+        EXECUTE IMMEDIATE 'GRANT EXECUTE ON PROJECT_USER.' || proc_record.object_name || ' TO ANALYST_USER';
+    END LOOP;
+
+    -- Grant EXECUTE privileges on all packages in the PROJECT_USER schema
+    FOR package_record IN (SELECT object_name FROM all_objects WHERE object_type = 'PACKAGE' AND owner = 'PROJECT_USER') LOOP
+        EXECUTE IMMEDIATE 'GRANT EXECUTE ON PROJECT_USER.' || package_record.object_name || ' TO ANALYST_USER';
+    END LOOP;
+
+    -- Grant EXECUTE privileges on all package bodies in the PROJECT_USER schema
+    FOR package_body_record IN (SELECT object_name FROM all_objects WHERE object_type = 'PACKAGE BODY' AND owner = 'PROJECT_USER') LOOP
+        EXECUTE IMMEDIATE 'GRANT EXECUTE ON PROJECT_USER.' || package_body_record.object_name || ' TO ANALYST_USER';
+    END LOOP;
+
 EXCEPTION
     WHEN OTHERS THEN
-        NULL; -- Ignore if grants already exist
+        NULL; -- Ignore errors if privileges already exist or if something else goes wrong
 END;
 /
